964c9857792310be0db886413ffa603945e0e06a
Kevin <kevin.06.huang@gmail.com>
2023-07-18 18:41:19 -0700

    Port over navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="bg-ct-blue-600 min-h-screen">
       <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-        <p className="text-3xl font-semibold">Welcome to 2FA with Next.js</p>
+        <p className="text-3xl font-semibold">PASSKEY</p>
       </div>
     </section>
   );
